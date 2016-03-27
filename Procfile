d723d894ea6ff6afb38272d5f7c1463640a5e006
Laco Gubik <lacogubik@gmailcom>
2016-03-27 08:14:50 +0100

    init

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/cierne-standalone.jar clojure.main -m cierne.web
---
+++ test/cierne/web_test.clj
@@ -0,0 +1,6 @@
+(ns cierne.web-test
+  (:require [clojure.test :refer :all]
+            [cierne.web :refer :all]))
+
+(deftest first-test
+  (is false "Tests should be written"))
