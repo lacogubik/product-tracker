b809a337320907e407821bce238f4c4b6ad384f0
Ladislav Gubik <lacogubik@gmail.com>
2016-04-09 23:49:37 +0100

    Updating author list

--- src/cierne/find.clj
+++ src/cierne/find.clj
@@ -7,51 +7,67 @@
 (def page-batch 2)
 
 
-(def wanted-authors #{"andruchovyč"
-                      "andrejev"
-                      "alighieri"                           ;bozska komedia
+(def wanted-authors #{"andruchovyč"                         ;rekreacie, moskoviada atd
+                      "alexijevičová"                       ;zinkovy chlapci, vojna nema zensku tvar, modlidba za cernobyl
+                      "andrejev"                            ;Propast a jine povidky, poviedka o siedmych obesenych
+                      "bartošová"                           ;Napriek totalite
+                      "baláž"                               ;Posledná pevnosť
                       "belyj"                               ;peterburg
-                      "beckett"                             ;molloy
+                      "beckett"                             ;molloy, Prvá láska a iné prózy
+                      "bellow"                              ;Sebrané povídky I. & II.
                       "bernanos"                            ;dennik vidieckeho knaza
-                      "bondy"
                       "borges"                              ;spisy, cela kniznica
                       "canetti"                             ;zaslepeni
                       "carpentier"                          ;stratene kroky
-                      "dostojevskij"
-                      "dušek"
-                      "földvári"
-                      "gombrowicz"                          ;kosmos
+                      "cinger"                              ;Voskovec a Werich. Dialogy přes železnou oponu
+                      "dostojevskij"                        ;Bratia Karamazovci
+                      "dušek"                               ;Melon sa vzdy smeje
+                      "földvári"                            ;...až pod čiernu zem
+                      "gombrowicz"                          ;kosmos, pornografia
+                      "grečner"                             ;Film ako volny vers
+                      "hesse"                               ;Stepný vlk, + ine
+                      "hofstadter"                          ;Gödel, Escher, Bach. Existenciální gordická balada
                       "jančar"                              ;Drago, Kateřina, páv a jezuita
-                      "karous"
+                      "jakubisko"                           ;Zive striebro
+                      "karous"                              ;Vetřelci a volavky
+                      "krajňak"                             ;Carpathia
+                      "kupka"                               ;Ruská moderna, Ruská avangarda
                       "kameníček"
                       "kiš"                                 ;encyklopedia mrtvych, hrobka borisa davidovica
                       "lasica"
-                      "márai"
+                      "makine"                              ;Francuzky testament
+                      "mathé"                               ;Ján Mathé. Hľadač dobra
+                      "matuštík"                            ;Ján Mathé. Hľadač dobra
+                      "márai"                               ;Spoveď mešťana
                       "marquez"
-                      "mitana"
+                      "mitana"                              ;Psie dni
+                      "mojžiš"                              ;Voľným okom
                       "morante"                             ;pribeh v dejinach
+                      "myšľanov"                            ;Sadomaso, fetiš a Tony
                       "musil"                               ;muz bez  vlastnosti
                       "nabokov"                             ;pozvanie na popravu, lolita
-                      "pecka"
+                      "pain"                                ;more love cajky
                       "pišťanek"
                       "platonov"                            ;cevengur
+                      "ragač"                               ;August 68
                       "rakús"                               ;Mačacia krajina
+                      "rosová"                              ;Male vianoce
                       "satinský"
                       "sabato"                              ;tunel samoty
                       "saramago"                            ;mesto slepych
-                      "sartre"                              ;hnus a ine
+                      "sartre"                              ;nevolnost, hnus a ine
                       "singer"                              ;kejklir z lubliny
                       "sloboda"
                       "solženicyn"                          ;prvy kruh, polostrov gulag
-                      "staviarsky"
+                      "staviarsky"                          ;Zachytka, Kale topanky, Clovek prijemny
                       "šimečka"
-                      "suk"                                 ;Labyrintem revoluce
-                      "štrpka"
-                      "tatarka"
-                      "tournier"                            ;kral tmy
+                      "štrpka"                              ;Rukojemnik
+                      "tatarka"                             ;Navrávačky, Písačky pre milovanú Lutéciu
+                      "tournier"                            ;kral tmy/kral duchov,
                       "ulická"                              ;medea a jej deti
-                      "vaculík"                             ;morcata
-                      "vilikovský"
+                      "vaculík"                             ;morcata, jsme v nebi
+                      "vilikovský"                          ;Vlastnty zivotopis zla, Pes na ceste, Letmy sneh, Prva a posledna laska
+                      "werfel"                              ;Hvězda nenarozených, Barbora neboli zbožnost
                       })
 
 (def stop-processing?* (atom false))
@@ -78,6 +94,23 @@
       (map #(assoc % :author (second (:author %))))
       pprint-map->)))
 
+
+;(defn contains-book?
+;  [last-book new-page]
+;  (reduce (fn [book-exists? book]
+;            (if (= (:url last-book) (:url book))
+;              true
+;              book-exists?)) new-page))
+;
+;(defn get-latest2
+;  []
+;  (let [last-book (db/get-data)]
+;    (loop [n 0
+;           pages '()]
+;      (let [new-page (get-wanted-books n)]
+;        (if (or (contains-book? last-book new-page) (> n page-batch))
+;          (conj pages new-page)
+;          (recur (inc n) (conj pages new-page)))))))
 
 (defn get-latest
   []
