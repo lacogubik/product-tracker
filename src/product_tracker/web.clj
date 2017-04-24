82e231112fa3d4011e7b6e02ed21f47d7c40135e
Laco Gubik <lacogubik@gmailcom>
2017-04-25 00:01:53 +0100

    Added slashes

--- src/product_tracker/web.clj
+++ src/product_tracker/web.clj
@@ -17,15 +17,11 @@
              {:status  200
               :headers {"Content-Type" "text/plain"}
               :body    (pr-str (n/send-msg (find-wanted)))})
-           (GET "/article.html" []
-             {:status  200
-              :headers {"Content-Type" "text/html"}
-              :body    (slurp (io/resource "article.html"))})
            (GET "/article.html/" []
              {:status  200
               :headers {"Content-Type" "text/html"}
               :body    (slurp (io/resource "article.html"))})
-           (GET "/article.amp.html" []
+           (GET "/article.amp.html/" []
              {:status  200
               :headers {"Content-Type" "text/html"}
               :body    (slurp (io/resource "article.amp.html"))})
