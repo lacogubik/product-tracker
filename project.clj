4b449d17704f6707b07f6d10f2d44cceef2a8c08
Laco Gubík <lacogubik@gmail.com>
2017-10-08 23:34:23 +0100

    Add Sentry

--- project.clj
+++ project.clj
@@ -5,17 +5,17 @@
                  [compojure "1.1.8"]
                  [ring/ring-jetty-adapter "1.2.2"]
                  [ring/ring-devel "1.2.2"]
-                 [environ "0.5.0"]
+                 [environ "1.1.0"]
                  [reaver "0.1.2"]
                  [clj-http "2.1.0"]
                  [prismatic/schema "1.1.3"]
-                 [com.taoensso/timbre "4.7.4"]
+                 [com.taoensso/timbre "4.10.0"]
                  [hiccup "1.0.5"]
-                 [cheshire "5.5.0"]]
+                 [cheshire "5.5.0"]
+                 [raven-clj "1.5.0"]]
 
-  :min-lein-version "2.0.0"
-  :plugins [[environ/environ.lein "0.2.1"]]
-  :hooks [environ.leiningen.hooks]
+  :min-lein-version "2.7.0"
+  :plugins [[lein-environ "1.1.0"]]
   :main product-tracker.web
   :uberjar-name "product-tracker-standalone.jar"
 
--- src/product_tracker/web.clj
+++ src/product_tracker/web.clj
@@ -1,13 +1,21 @@
 (ns product-tracker.web
   (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
             [compojure.route :as route]
+            [compojure.handler :as handler]
             [clojure.java.io :as io]
             [ring.middleware.stacktrace :as trace]
             [ring.adapter.jetty :as jetty]
             [environ.core :refer [env]]
             [product-tracker.find :refer [find-wanted]]
-            [product-tracker.notification :as n]))
+            [product-tracker.notification :as n]
+            [raven-clj.ring :as rvn]
+            [taoensso.timbre.appenders.3rd-party.sentry :as sentry]
+            [taoensso.timbre :as log]))
 
+(def sentry-dsn (env :sentry-dsn))
+(def environment (env :environment))
+
+(log/merge-config! {:appenders {:sentry (sentry/sentry-appender sentry-dsn {:environment environment})}})
 
 (defroutes app
            (GET "/" []
@@ -27,13 +35,20 @@
 
 (defn wrap-app [app]
   (-> app
-      ((if (= "production" (env :environment))
+      (rvn/wrap-sentry sentry-dsn {:environment environment})
+      ((if (= "production" environment)
          wrap-error-page
-         trace/wrap-stacktrace))))
+         trace/wrap-stacktrace))
+      (handler/site)))
 
 
 (defn -main [& args]
   (let [port (-> (env :port)
                  (or "8080")
                  Integer/parseInt)]
+    (println "ENV:" environment)
+    (Thread/setDefaultUncaughtExceptionHandler
+      (reify Thread$UncaughtExceptionHandler
+        (uncaughtException [_ thread ex]
+          (log/error ex "Uncaught exception on" (.getName thread)))))
     (jetty/run-jetty (wrap-app #'app) {:port port})))
